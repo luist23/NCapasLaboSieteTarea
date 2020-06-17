4758597db5584a0bf5212622342a6b213bfc3e3e
luist23 <00127717@uca.edu.sv>
2020-06-16 21:43:50 -0600

    add service y repository

--- src/main/java/com/uca/capas/config/JPAConfiguration.java
+++ src/main/java/com/uca/capas/config/JPAConfiguration.java
@@ -5,12 +5,16 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "com.uca.capas.repositories")
 public class JPAConfiguration {
 	
 	@Bean
---
+++ src/main/java/com/uca/capas/service/EstudianteService.java
@@ -0,0 +1,21 @@
+package com.uca.capas.service;
+
+import java.util.List;
+import org.springframework.dao.DataAccessException;
+import com.uca.capas.domain.Estudiante;
+
+public interface EstudianteService {
+	
+	public List<Estudiante> findAll() throws DataAccessException;
+	
+	public Estudiante findOne(Integer id) throws DataAccessException;
+	
+	public void insert(Estudiante estudiante) throws DataAccessException;
+	
+	public void delete(Integer codigo) throws DataAccessException;
+
+	public List<Estudiante> filtrarPor(String cadena) throws DataAccessException;
+	
+	public List<Estudiante> empiezaCon(String cadena) throws DataAccessException;
+
+}
